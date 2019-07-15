02e886134cf504733571c8616f68431c7c2ee4ab
Jakob Otto <jakob.otto@haw-hamburg.de>
2019-07-15 16:18:07 +0200

    Add wifi-component

--- main/my_main.c
+++ main/my_main.c
@@ -1,4 +1,4 @@
-/* Blink Example
+/* WiFi station Example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -6,12 +6,29 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "wifi.h"
+#include "/home/boss/Arduino/libraries/creds/c.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 void app_main() {
-  printf("Hello World\n\r");
-  fflush(stdout);
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-  esp_restart();
+    ESP_LOGI("main", "ESP_WIFI_MODE_STA");
+    printf("BLOCKING CONNECT call now!\n");
+    wifi_connect(SSID, PASS);
+    printf("BLOCKING CONNECT I'm back!\n");
 }
